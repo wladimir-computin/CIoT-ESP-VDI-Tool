75ecb183cd9b0e5f515333bc6176b27313cf5ef1
David Wischnjak <davidw.software@gmail.com>
2025-04-03 12:01:35 +0200

    first PoC

--- src/KB_App.cpp
+++ src/KB_App.cpp
@@ -6,6 +6,7 @@
 
 #include "KB_App.h"
 
+
 KB_App::KB_App(const char * name) {
   appname = name;
   registerCommand(COMMAND_KB_PRINT);
@@ -13,12 +14,17 @@ KB_App::KB_App(const char * name) {
   registerCommand(COMMAND_KB_HIT);
   registerCommand(COMMAND_KB_PRESS);
   registerCommand(COMMAND_KB_RELEASE);
+  registerCommand(COMMAND_KB_LAYOUT);
+  registerCommand(COMMAND_KB_SLEEP);
 }
 
 void KB_App::setup() {
   PersistentMemory pmem(appname, true);
+  String layout = pmem.readString("layout", "de_DE");
   pmem.commit();
-  Keyboard.begin();
+  Keyboard.begin(layoutFromString(layout.c_str()));
+
+  USB.begin();
 
 }
 
@@ -34,11 +40,13 @@ String KB_App::getType() {
 ProcessMessageStruct KB_App::processMessage(String &message) {
   if (COMMAND_KB_PRINT.check(message)) {
     String val = COMMAND_KB_PRINT.getParamVal(message, 0);
+    Keyboard.print(val);
     return {ACK, "", FLAG_KEEP_ALIVE};
   }
 
   if (COMMAND_KB_PRINTLN.check(message)) {
     String val = COMMAND_KB_PRINT.getParamVal(message, 0);
+    Keyboard.println(val);
     return {ACK, "", FLAG_KEEP_ALIVE};
   }
 
@@ -63,6 +71,12 @@ ProcessMessageStruct KB_App::processMessage(String &message) {
     return {ACK, "", FLAG_KEEP_ALIVE};
   }
 
+  if (COMMAND_KB_SLEEP.check(message)) {
+    int duration = COMMAND_KB_SLEEP.getParamVal(message, 0).toInt();
+    delay(duration);
+    return {ACK, "", FLAG_KEEP_ALIVE};
+  }
+
   return App::processMessage(message);
 }
 
@@ -71,4 +85,26 @@ void KB_App::loop() {
 
 String KB_App::getName() {
   return appname;
+}
+
+const uint8_t * KB_App::layoutFromString(const char * layout){
+  KB_Layout kb_layouts[10] = {
+    {"de_DE", KeyboardLayout_de_DE},
+    {"en_US", KeyboardLayout_en_US},
+    {"es_ES", KeyboardLayout_es_ES},
+    {"fr_FR", KeyboardLayout_fr_FR},
+    {"it_IT", KeyboardLayout_it_IT},
+    {"pt_PT", KeyboardLayout_pt_PT},
+    {"sv_SE", KeyboardLayout_sv_SE},
+    {"da_DK", KeyboardLayout_da_DK},
+    {"hu_HU", KeyboardLayout_hu_HU},
+    {"pt_BR", KeyboardLayout_pt_BR}
+  };
+
+  for (KB_Layout kb_layout : kb_layouts){
+    if(strcmp(kb_layout.name, layout) == 0){
+      return kb_layout.layout;
+    }
+  }
+  return KeyboardLayout_de_DE;
 }
--- src/KB_App.h
+++ src/KB_App.h
@@ -17,21 +17,43 @@
 #include <USB.h>
 #include <USBHIDKeyboard.h>
 
+
 static Param COMMAND_KB_PRINT_PARAMS[] = {{"string", DATATYPE_STRING, false, 1, 1024}};
 static Command COMMAND_KB_PRINT("print", ARRAY_LEN(COMMAND_KB_PRINT_PARAMS), COMMAND_KB_PRINT_PARAMS);
 static Command COMMAND_KB_PRINTLN("println", ARRAY_LEN(COMMAND_KB_PRINT_PARAMS), COMMAND_KB_PRINT_PARAMS);
 
-static Param COMMAND_KB_HIT_PARAMS[] = {{"string", DATATYPE_STRING, false, 1, 64}, {"duration", DATATYPE_INT, true, 1, 1000}};
+static Param COMMAND_KB_HIT_PARAMS[] = {{"keys", DATATYPE_STRING, false, 1, 64}, {"duration", DATATYPE_INT, true, 1, 1000}};
 static Command COMMAND_KB_HIT("hit", ARRAY_LEN(COMMAND_KB_HIT_PARAMS), COMMAND_KB_HIT_PARAMS);
 
-static Param COMMAND_KB_PRESS_PARAMS[] = {{"string", DATATYPE_STRING, false, 1, 64}};
+static Param COMMAND_KB_PRESS_PARAMS[] = {{"keys", DATATYPE_STRING, false, 1, 64}};
 static Command COMMAND_KB_PRESS("press", ARRAY_LEN(COMMAND_KB_PRESS_PARAMS), COMMAND_KB_PRESS_PARAMS);
 
-static Param COMMAND_KB_RELEASE_PARAMS[] = {{"string", DATATYPE_STRING, true, 1, 64}};
+static Param COMMAND_KB_RELEASE_PARAMS[] = {{"keys", DATATYPE_STRING, true, 1, 64}};
 static Command COMMAND_KB_RELEASE("release", ARRAY_LEN(COMMAND_KB_RELEASE_PARAMS), COMMAND_KB_RELEASE_PARAMS);
 
-static Param COMMAND_KB_LAYOUT_PARAMS[] = {{"string", DATATYPE_STRING, true, 1, 10}};
-static Command COMMAND_KB_LAYOUT("release", ARRAY_LEN(COMMAND_KB_LAYOUT_PARAMS), COMMAND_KB_LAYOUT_PARAMS);
+static Param COMMAND_KB_LAYOUT_PARAMS[] = {{"layout", DATATYPE_STRING, true, 1, 10}};
+static Command COMMAND_KB_LAYOUT("layout", ARRAY_LEN(COMMAND_KB_LAYOUT_PARAMS), COMMAND_KB_LAYOUT_PARAMS);
+
+static Param COMMAND_KB_SLEEP_PARAMS[] = {{"millies", DATATYPE_INT, false, 0, 1000*60*10}}; //10 minutes
+static Command COMMAND_KB_SLEEP("sleep", ARRAY_LEN(COMMAND_KB_SLEEP_PARAMS), COMMAND_KB_SLEEP_PARAMS);
+
+struct KB_Layout {
+  const char name[10];
+  const uint8_t * layout;
+};
+
+// Supported keyboard layouts
+extern const uint8_t KeyboardLayout_de_DE[];
+extern const uint8_t KeyboardLayout_en_US[];
+extern const uint8_t KeyboardLayout_es_ES[];
+extern const uint8_t KeyboardLayout_fr_FR[];
+extern const uint8_t KeyboardLayout_it_IT[];
+extern const uint8_t KeyboardLayout_pt_PT[];
+extern const uint8_t KeyboardLayout_sv_SE[];
+extern const uint8_t KeyboardLayout_da_DK[];
+extern const uint8_t KeyboardLayout_hu_HU[];
+extern const uint8_t KeyboardLayout_pt_BR[];
+
 
 class KB_App : public App{
 
@@ -47,8 +69,12 @@ public:
 
 private:
   String appname;
-
   USBHIDKeyboard Keyboard;
+
+
+  const uint8_t * layoutFromString(const char * layout);
+
+
 
   //static void geigerClick();
   //Ticker geigerTicker;
