0cb1988a7e26f4bd7a7a74d4deb71934c575179e
David Wischnjak <davidw.software@gmail.com>
2025-06-16 05:50:06 +0200

    PoC

--- src/KB_App.cpp
+++ src/KB_App.cpp
@@ -6,7 +6,6 @@
 
 #include "KB_App.h"
 
-
 KB_App::KB_App(const char * name) {
   appname = name;
   registerCommand(COMMAND_KB_PRINT);
@@ -14,22 +13,46 @@ KB_App::KB_App(const char * name) {
   registerCommand(COMMAND_KB_HIT);
   registerCommand(COMMAND_KB_PRESS);
   registerCommand(COMMAND_KB_RELEASE);
+  registerCommand(COMMAND_KB_LEDS);
   registerCommand(COMMAND_KB_LAYOUT);
   registerCommand(COMMAND_KB_SLEEP);
 }
 
 void KB_App::setup() {
   PersistentMemory pmem(appname, true);
-  String layout = pmem.readString("layout", "de_DE");
+  String layout = pmem.readString("layout", current_kb_layout->name);
+  shift_key_reports =  pmem.readBool("shift_key_reports", shift_key_reports);
   pmem.commit();
-  Keyboard.begin(layoutFromString(layout.c_str()));
-
+  current_kb_layout = layoutFromString(layout.c_str());
+  Keyboard.begin(current_kb_layout->layout);
+  Keyboard.setShiftKeyReports(shift_key_reports);
+  Keyboard.onEvent(ARDUINO_USB_HID_KEYBOARD_LED_EVENT, keyboardLedCallback);
   USB.begin();
-
 }
 
 String KB_App::getStatus() {
-  return "";
+  char formats[] =
+  "Current KB Layout: %s\n"
+  "Shift Key Reports: %d\n"
+  "\n"
+  "Num Lock: %d\n"
+  "Caps Lock: %d\n"
+  "Scroll Lock: %d\n"
+  "Compose: %d\n"
+  "Kana: %d\n"
+  ;
+
+  char buf[512];
+  snprintf(buf, sizeof(buf) / sizeof(buf[0]), formats,
+           current_kb_layout->name,
+           shift_key_reports,
+           current_leds.numlock,
+           current_leds.capslock,
+           current_leds.scrolllock,
+           current_leds.compose,
+           current_leds.kana
+  );
+  return buf;
 }
 
 String KB_App::getType() {
@@ -53,22 +76,151 @@ ProcessMessageStruct KB_App::processMessage(String &message) {
   if (COMMAND_KB_HIT.check(message)) {
     String val = COMMAND_KB_HIT.getParamVal(message, 0);
     int duration = COMMAND_KB_HIT.getParamVal(message, 1).toInt();
-    return {ACK, "", FLAG_KEEP_ALIVE};
+
+    if(duration == 0){
+      duration = 100;
+    }
+
+    // Split the input by '+' to handle key combinations
+    int keyCount = 0;
+    uint8_t keys[10]; // Maximum 10 keys in combination
+    bool validKeys = true;
+
+    // Count number of '+' to determine key count
+    int plusCount = 0;
+    for(int i = 0; i < val.length(); i++) {
+      if(val.charAt(i) == '+') plusCount++;
+    }
+    keyCount = plusCount + 1;
+
+    // Parse each key
+    int startPos = 0;
+    for(int i = 0; i < keyCount; i++) {
+      int endPos = val.indexOf('+', startPos);
+      if(endPos == -1) endPos = val.length(); // Last key
+
+      String keyStr = val.substring(startPos, endPos);
+      keyStr.trim(); // Remove any whitespace
+
+      uint8_t key = 0;
+      if(keyStr.length() == 1) {
+        // Single character key
+        key = keyStr[0];
+      } else {
+        // Look up in kb_keys map
+        key = kb_keys[keyStr];
+      }
+
+      if(key == 0) {
+        validKeys = false;
+        break;
+      }
+
+      keys[i] = key;
+      startPos = endPos + 1;
+    }
+
+    if(validKeys) {
+      // Press all keys
+      for(int i = 0; i < keyCount; i++) {
+        Keyboard.press(keys[i]);
+        delay(duration);
+      }
+
+      delay(duration);
+
+      // Release all keys
+      Keyboard.releaseAll();
+
+      return {ACK, "", FLAG_KEEP_ALIVE};
+    } else {
+      return {ERR, "INVALID_KEYCODE"};
+    }
   }
 
   if (COMMAND_KB_PRESS.check(message)) {
     String val = COMMAND_KB_PRESS.getParamVal(message, 0);
-    return {ACK, "", FLAG_KEEP_ALIVE};
+    uint8_t key = 0;
+    if(val.length() == 1){
+      key = val[0];
+    } else {
+      key = kb_keys[val];
+    }
+    if(key != 0){
+      Keyboard.press(key);
+      return {ACK, "", FLAG_KEEP_ALIVE};
+    } else {
+      return {ERR, "INVALID_KEYCODE"};
+    }
   }
 
   if (COMMAND_KB_RELEASE.check(message)) {
     String val = COMMAND_KB_RELEASE.getParamVal(message, 0);
+    if(val != ""){
+      uint8_t key = 0;
+      if(val.length() == 1){
+        key = val[0];
+      } else {
+        key = kb_keys[val];
+      }
+      if(key != 0){
+        Keyboard.press(key);
+        return {ACK, "", FLAG_KEEP_ALIVE};
+      } else {
+        return {ERR, "INVALID_KEYCODE"};
+      }
+    } else {
+      Keyboard.releaseAll();
+    }
     return {ACK, "", FLAG_KEEP_ALIVE};
+  }
+
+  if (COMMAND_KB_LEDS.check(message)) {
+    String led = COMMAND_KB_LEDS.getParamVal(message, 0);
+    String val = COMMAND_KB_LEDS.getParamVal(message, 1);
+    if(led != "" && val != ""){
+      return {ACK, "", FLAG_KEEP_ALIVE};
+    } else if (led != "") {
+      return {ACK, "", FLAG_KEEP_ALIVE};
+    } else {
+      JsonDocument json;
+      JsonObject jsonObj = json.to<JsonObject>();
+      jsonObj["NUM_LOCK"] = current_leds.numlock;
+      jsonObj["CAPS_LOCK"] = current_leds.capslock;
+      jsonObj["SCROLL_LOCK"] = current_leds.scrolllock;
+      jsonObj["COMPOSE"] = current_leds.compose;
+      jsonObj["KANA"] = current_leds.kana;
+
+      String out;
+      out.reserve(256);
+      serializeJsonPretty(json, out);
+      return {DATA, out, FLAG_KEEP_ALIVE};
+    }
   }
 
   if (COMMAND_KB_LAYOUT.check(message)) {
     String val = COMMAND_KB_LAYOUT.getParamVal(message, 0);
-    return {ACK, "", FLAG_KEEP_ALIVE};
+    if(val != ""){
+      PersistentMemory pmem(appname, true);
+      pmem.writeString("layout", layoutFromString(val.c_str())->name);
+      pmem.commit();
+      return {ACK, "", FLAG_KEEP_ALIVE};
+    } else {
+      JsonDocument json;
+      JsonArray jsonObj = json.to<JsonArray>();
+      for (KB_Layout &kb_layout : kb_layouts){
+        if(kb_layout.layout == current_kb_layout->layout){
+          jsonObj.add("[" + String(kb_layout.name) + "]");
+        } else {
+          jsonObj.add(kb_layout.name);
+        }
+      }
+
+      String out;
+      out.reserve(256);
+      serializeJsonPretty(json, out);
+      return {DATA, out, FLAG_KEEP_ALIVE};
+    }
   }
 
   if (COMMAND_KB_SLEEP.check(message)) {
@@ -80,6 +232,13 @@ ProcessMessageStruct KB_App::processMessage(String &message) {
   return App::processMessage(message);
 }
 
+
+// Instance method to handle the LED event
+void KB_App::keyboardLedCallback(void* handler_args, esp_event_base_t base, int32_t id, void* event_data) {
+  arduino_usb_hid_keyboard_event_data_t* led_data = static_cast<arduino_usb_hid_keyboard_event_data_t*>(event_data);
+  current_leds = *led_data;
+}
+
 void KB_App::loop() {
 }
 
@@ -87,24 +246,11 @@ String KB_App::getName() {
   return appname;
 }
 
-const uint8_t * KB_App::layoutFromString(const char * layout){
-  KB_Layout kb_layouts[10] = {
-    {"de_DE", KeyboardLayout_de_DE},
-    {"en_US", KeyboardLayout_en_US},
-    {"es_ES", KeyboardLayout_es_ES},
-    {"fr_FR", KeyboardLayout_fr_FR},
-    {"it_IT", KeyboardLayout_it_IT},
-    {"pt_PT", KeyboardLayout_pt_PT},
-    {"sv_SE", KeyboardLayout_sv_SE},
-    {"da_DK", KeyboardLayout_da_DK},
-    {"hu_HU", KeyboardLayout_hu_HU},
-    {"pt_BR", KeyboardLayout_pt_BR}
-  };
-
-  for (KB_Layout kb_layout : kb_layouts){
-    if(strcmp(kb_layout.name, layout) == 0){
-      return kb_layout.layout;
+KB_Layout * KB_App::layoutFromString(const char * name){
+  for (KB_Layout &kb_layout : kb_layouts){
+    if(strcmp(kb_layout.name, name) == 0){
+      return &kb_layout;
     }
   }
-  return KeyboardLayout_de_DE;
+  return &kb_layouts[0];
 }
--- src/KB_App.h
+++ src/KB_App.h
@@ -9,6 +9,7 @@
 #include <Arduino.h>
 #include <Ticker.h>
 #include <ArduinoJson.h>
+#include <map>
 
 #include "AllConfig.h"
 #include <App.h>
@@ -31,6 +32,9 @@ static Command COMMAND_KB_PRESS("press", ARRAY_LEN(COMMAND_KB_PRESS_PARAMS), COM
 static Param COMMAND_KB_RELEASE_PARAMS[] = {{"keys", DATATYPE_STRING, true, 1, 64}};
 static Command COMMAND_KB_RELEASE("release", ARRAY_LEN(COMMAND_KB_RELEASE_PARAMS), COMMAND_KB_RELEASE_PARAMS);
 
+static Param COMMAND_KB_LEDS_PARAMS[] = {{"led", DATATYPE_STRING, true, 1, 32}, {"value", DATATYPE_BOOL, true}};
+static Command COMMAND_KB_LEDS("leds", ARRAY_LEN(COMMAND_KB_LEDS_PARAMS), COMMAND_KB_LEDS_PARAMS);
+
 static Param COMMAND_KB_LAYOUT_PARAMS[] = {{"layout", DATATYPE_STRING, true, 1, 10}};
 static Command COMMAND_KB_LAYOUT("layout", ARRAY_LEN(COMMAND_KB_LAYOUT_PARAMS), COMMAND_KB_LAYOUT_PARAMS);
 
@@ -40,6 +44,11 @@ static Command COMMAND_KB_SLEEP("sleep", ARRAY_LEN(COMMAND_KB_SLEEP_PARAMS), COM
 struct KB_Layout {
   const char name[10];
   const uint8_t * layout;
+};
+
+struct KB_Keys {
+  const char name[10];
+  const uint8_t keycode;
 };
 
 // Supported keyboard layouts
@@ -54,6 +63,83 @@ extern const uint8_t KeyboardLayout_da_DK[];
 extern const uint8_t KeyboardLayout_hu_HU[];
 extern const uint8_t KeyboardLayout_pt_BR[];
 
+static KB_Layout kb_layouts[] = {
+  {"de_DE", KeyboardLayout_de_DE},
+  {"en_US", KeyboardLayout_en_US},
+  {"es_ES", KeyboardLayout_es_ES},
+  {"fr_FR", KeyboardLayout_fr_FR},
+  {"it_IT", KeyboardLayout_it_IT},
+  {"pt_PT", KeyboardLayout_pt_PT},
+  {"sv_SE", KeyboardLayout_sv_SE},
+  {"da_DK", KeyboardLayout_da_DK},
+  {"hu_HU", KeyboardLayout_hu_HU},
+  {"pt_BR", KeyboardLayout_pt_BR}
+};
+
+static std::map<String, uint8_t> kb_keys = {
+  {"L_CTRL"      , KEY_LEFT_CTRL    },
+  {"L_SHIFT"     , KEY_LEFT_SHIFT   },
+  {"L_ALT"       , KEY_LEFT_ALT     },
+  {"L_GUI"       , KEY_LEFT_GUI     },
+  {"R_CTRL"      , KEY_RIGHT_CTRL   },
+  {"R_SHIFT"     , KEY_RIGHT_SHIFT  },
+  {"R_ALT"       , KEY_RIGHT_ALT    },
+  {"R_GUI"       , KEY_RIGHT_GUI    },
+
+  {"UP"          , KEY_UP_ARROW     },
+  {"DOWN"        , KEY_DOWN_ARROW   },
+  {"LEFT"        , KEY_LEFT_ARROW   },
+  {"RIGHT"       , KEY_RIGHT_ARROW  },
+  {"MENU"        , KEY_MENU         },
+  {"SPACE"       , KEY_SPACE        },
+  {"BACKSPACE"   , KEY_BACKSPACE    },
+  {"TAB"         , KEY_TAB          },
+  {"RETURN"      , KEY_RETURN       },
+  {"ESC"         , KEY_ESC          },
+  {"INSERT"      , KEY_INSERT       },
+  {"DELETE"      , KEY_DELETE       },
+  {"PAGE_UP"     , KEY_PAGE_UP      },
+  {"PAGE_DOWN"   , KEY_PAGE_DOWN    },
+  {"HOME"        , KEY_HOME         },
+  {"END"         , KEY_END          },
+  {"NUM_LOCK"    , KEY_NUM_LOCK     },
+  {"CAPS_LOCK"   , KEY_CAPS_LOCK    },
+  {"F1"          , KEY_F1           },
+  {"F2"          , KEY_F2           },
+  {"F3"          , KEY_F3           },
+  {"F4"          , KEY_F4           },
+  {"F5"          , KEY_F5           },
+  {"F6"          , KEY_F6           },
+  {"F7"          , KEY_F7           },
+  {"F8"          , KEY_F8           },
+  {"F9"          , KEY_F9           },
+  {"F10"         , KEY_F10          },
+  {"F11"         , KEY_F11          },
+  {"F12"         , KEY_F12          },
+  {"F13"         , KEY_F13          },
+  {"F14"         , KEY_F14          },
+  {"F15"         , KEY_F15          },
+  {"F16"         , KEY_F16          },
+  {"F17"         , KEY_F17          },
+  {"F18"         , KEY_F18          },
+  {"F19"         , KEY_F19          },
+  {"F20"         , KEY_F20          },
+  {"F21"         , KEY_F21          },
+  {"F22"         , KEY_F22          },
+  {"F23"         , KEY_F23          },
+  {"F24"         , KEY_F24          },
+  {"PRINT_SCREEN", KEY_PRINT_SCREEN },
+  {"SCROLL_LOCK" , KEY_SCROLL_LOCK  },
+  {"PAUSE"       , KEY_PAUSE        },
+
+  {"CTRL"        , KEY_LEFT_CTRL    },
+  {"SHIFT"       , KEY_LEFT_SHIFT   },
+  {"ALT"         , KEY_LEFT_ALT     },
+  {"GUI"         , KEY_LEFT_GUI     },
+  {"ENTER"       , KEY_RETURN       }
+};
+
+static arduino_usb_hid_keyboard_event_data_t current_leds;
 
 class KB_App : public App{
 
@@ -70,9 +156,11 @@ public:
 private:
   String appname;
   USBHIDKeyboard Keyboard;
+  KB_Layout * current_kb_layout = &kb_layouts[0];
+  bool shift_key_reports = true;
+  static void keyboardLedCallback(void* handler_args, esp_event_base_t base, int32_t id, void* event_data);
 
-
-  const uint8_t * layoutFromString(const char * layout);
+  KB_Layout * layoutFromString(const char * name);
 
 
 
